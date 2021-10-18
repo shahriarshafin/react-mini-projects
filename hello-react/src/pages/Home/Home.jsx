b64736eafef62ccb45eea581138777ef41de7111
shafin <shafinshahriar340@gmail.com>
2021-10-18 21:06:22 +0600

    js expressions

--- hello-react/src/pages/Home/Home.jsx
+++ hello-react/src/pages/Home/Home.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import Footer from '../../components/Footer/Footer';
 import Nav from '../../components/Nav/Nav';
 import './Home.css';
+
+const home = 'Home';
+const date = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
+
 const Home = () => {
 	return (
 		<div>
 			<Nav />
-			<h1>Home Sweet Home!</h1>
+			<h1>Home Sweet {home} </h1>
+			<p>
+				Today's Date : <span> {date} </span>
+			</p>
+			<p>
+				Clock Now : <span> {time} </span>
+			</p>
 			<Footer />
 		</div>
 	);
