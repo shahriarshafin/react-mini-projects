2562dae331d4b4c322d7c7804a1041872dfcf011
shafin <shafinshahriar340@gmail.com>
2021-10-20 03:22:39 +0600

    added cardData, Card style changed

--- hello-react/src/components/Card/Card.jsx
+++ hello-react/src/components/Card/Card.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
-import UserImg from '../../assets/images/user.png';
-import './Card.css';
+import '../../assets/data/data.json';
+import '../../bootstrap.css';
 
-const Card = (props) => {
+const Card = () => {
+	fetch('./data/data.json')
+		.then((res) => res.json())
+		.then((json) => console.table(json));
+	// console.log('hello');
+
+	console.log('hello');
+
 	return (
 		<div>
-			<div className="card">
-				<img src={UserImg} alt="Avatar" />
-				<div class="container">
-					<h4>
-						<b>{props.name}</b>
-					</h4>
-					<p>{props.status}</p>
+			<div className="col-6 col-lg-4 col-md-4">
+				<div className="snip-card mt-5 p-2">
+					<div className="d-flex justify-content-between">
+						<div>
+							<span className="badge bg-primary mb-2">bootstrap</span>
+							<a href="http//:">
+								<span className="badge bg-danger mb-2 mx-1">feature</span>
+							</a>
+						</div>
+						<div>
+							<span className="badge bg-success mb-2">free</span>
+						</div>
+					</div>
+					<a href="https://">
+						<div className="bg-dark">
+							<img
+								className="card-img img-fluid"
+								alt="click here"
+								loading="lazy"
+								src="https://snippet-treasury.netlify.app/assets/img/features/feature-6.png"
+							/>
+						</div>
+					</a>
+					<p className="snip-card-cap my-2">Lorem ipsum dolor sit amet.</p>
 				</div>
 			</div>
 		</div>
