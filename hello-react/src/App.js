97b178213ed2eb8a3f98a2713e396595dbbca0a0
shafin <shafinshahriar340@gmail.com>
2021-10-20 01:47:06 +0600

    added bootstrap in contact page

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { About, Contact, Error, Home } from './pages/export';
-
 function App() {
 	return (
 		<main>
