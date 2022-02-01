c2060c91cf7cda99869ec5938b3c97dd41c4c7ac
shafin <shafinshahriar340@gmail.com>
2022-02-01 13:57:07 +0600

    added page plagination

--- next-tailwindcss/pages/posts/index.js
+++ next-tailwindcss/pages/posts/index.js
@@ -1,32 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from '../../components/navbar';
 import PageHeading from '../../components/PageHeading';
 import PostCard from '../../components/PostCard';
-const pageNo = 1;
-export const getStaticProps = async () => {
-	let res = await fetch(
-		`https://jsonplaceholder.typicode.com/posts?_page= ${pageNo}&_limit=10`
-	);
-	const data = await res.json();
-	return {
-		props: { posts: data },
-	};
-};
 
-const handleClick = () => {
-	const [pageNo, SetpageNo] = useState(1);
-	console.log('hello');
-	return pageNo;
-};
+const index = () => {
+	const [postsToShow, setPostsToShow] = useState([]);
+	const [pageNo, setPageNo] = useState(1);
 
-const index = ({ posts }) => {
+	useEffect(async () => {
+		const response = await fetch(
+			`https://jsonplaceholder.typicode.com/posts?_page=${pageNo}`
+		);
+		const users = await response.json();
+		setPostsToShow(users);
+	}, [pageNo]);
+
 	return (
 		<>
 			<Navbar />
 			<main>
 				<PageHeading heading={'News Feed'} />
 				<div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
-					{posts.map((post) => (
+					{postsToShow.map((post) => (
 						<PostCard
 							key={post.id}
 							postNo={post.id}
@@ -40,7 +35,10 @@ const index = ({ posts }) => {
 					<nav aria-label='Page navigation'>
 						<ul className='inline-flex'>
 							<li>
-								<button className='h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-indigo-600 rounded-l-lg focus:shadow-outline hover:bg-indigo-100'>
+								<button
+									onClick={() => (pageNo > 1 ? setPageNo(pageNo - 1) : '')}
+									className='h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-indigo-600 rounded-l-lg focus:shadow-outline hover:bg-indigo-100'
+								>
 									Prev
 								</button>
 							</li>
@@ -50,10 +48,7 @@ const index = ({ posts }) => {
 								</button>
 							</li>
 							<li>
-								<button
-									onClick={handleClick}
-									className='h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-indigo-600 focus:shadow-outline hover:bg-indigo-100'
-								>
+								<button className='h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-indigo-600 focus:shadow-outline hover:bg-indigo-100'>
 									2
 								</button>
 							</li>
@@ -63,7 +58,10 @@ const index = ({ posts }) => {
 								</button>
 							</li>
 							<li>
-								<button className='h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white border border-indigo-600 rounded-r-lg focus:shadow-outline hover:bg-indigo-100'>
+								<button
+									onClick={() => setPageNo(pageNo + 1)}
+									className='h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white border border-indigo-600 rounded-r-lg focus:shadow-outline hover:bg-indigo-100'
+								>
 									Next
 								</button>
 							</li>
